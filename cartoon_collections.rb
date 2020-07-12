13b401924f85711e2ed9ad45b354dc23f226c0e1
Amanda Walker <walker.akw@gmail.com>
2020-07-12 23:57:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -27,5 +27,5 @@ end
 
 def find_the_cheese(find)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  if find.detect {|food| food.include?(chhese_type)}
+    find.detect {|food| food.include?(chhese_type)}
 end
