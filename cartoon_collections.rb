694c136343620b5a3b157eb7bc08bb93aced655c
Amanda Walker <walker.akw@gmail.com>
2020-07-12 23:56:39 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,7 +4,7 @@ def roll_call_dwarves(array)
   # array.each_with_index do |name, number|
   #   puts "#{number+1} #{name}"
   # end #do
-  array.each_with_index { |name, number| puts "#{number+1} #{name}" }
+  array.each_with_index { |name, number| puts "#{number+1} #{name}"}
 end
 #roll_call_dwarves(array)
 
@@ -26,5 +26,6 @@ def long_planeteer_calls(call_length)
 end
 
 def find_the_cheese(find)
-
+  cheese_types = ["cheddar", "gouda", "camembert"]
+  if find. {|food| food.include?(chhese_type)}
 end
